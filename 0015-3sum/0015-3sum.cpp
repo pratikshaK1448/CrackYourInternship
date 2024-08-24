7b90aa4246a6d1e1a77aad076b3620691722cf0c
pratikshaK1448 <97044744+pratikshaK1448@users.noreply.github.com>
2024-08-24 23:18:24 +0530

    Time: 57 ms (80.51%), Space: 27.5 MB (70.73%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,43 +1,34 @@
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> res;
-        sort(nums.begin(), nums.end());  // Step 1: Sort the array
-        int n = nums.size();
-        
-        for (int i = 0; i < n - 2; i++) {
-            // Skip duplicate values for the first element
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
             
-            int target = -nums[i];
-            int left = i + 1;
-            int right = n - 1;
-            
-            while (left < right) {
-                int sum = nums[left] + nums[right];
-                
-                if (sum == target) {
-                    res.push_back({nums[i], nums[left], nums[right]});
-                    
-                    // Skip duplicate values for the second and third elements
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-                    
-                    left++;
-                    right--;
-                } else if (sum < target) {
-                    left++;
+            int j = i + 1;
+            int k = nums.size() - 1;
+
+            while (j < k) {
+                int total = nums[i] + nums[j] + nums[k];
+
+                if (total > 0) {
+                    k--;
+                } else if (total < 0) {
+                    j++;
                 } else {
-                    right--;
+                    res.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+
+                    while (nums[j] == nums[j-1] && j < k) {
+                        j++;
+                    }
                 }
             }
         }
-        
-        return res;
+        return res;        
     }
 };
